bf2a226fd54ec326c6ba7ec04779280828d5c3b6
Dim <om1234777@gmail.com>
2020-08-30 22:41:22 +0300

    add rowBlock

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -42,7 +42,7 @@ export default class ItemList extends Component {
                 <li
                     key={id}
                     className="list-group-item"
-                    onClick={() => this.props.onCharSelected(id)}
+                    onClick={() => this.props.onItemSelected(id)}
                     >
                     {label}
                 </li>
